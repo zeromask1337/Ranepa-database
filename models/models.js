8f77d5665f0dfffa0e3f226a1b2e2698ba969cfa
Demonizer <vysmol@example.com>
2021-02-05 16:12:18 +0300

    Added constraints

--- models/models.js
+++ models/models.js
@@ -1,8 +1,8 @@
-const { DataTypes } = require("sequelize");
-const db = require("../config/database");
+const { DataTypes } = require("sequelize")
+const db = require("../config/database")
 
-const Clients = db.define(
-    "Clients",
+const Office = db.define(
+    "Office",
     {
         id: {
             type: DataTypes.INTEGER,
@@ -10,35 +10,14 @@ const Clients = db.define(
             primaryKey: true,
             autoIncrement: true,
         },
-        name: DataTypes.STRING(45),
-        total_sum: DataTypes.DECIMAL(20, 2),
+        address: DataTypes.STRING(45),
     },
     {
         // Other model options go here
-        tableName: "clients",
+        tableName: "office",
         timestamps: false,
     }
-);
-
-const Developer = db.define(
-    "Developer",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        role: DataTypes.STRING(45),
-        level: DataTypes.STRING(45),
-        project_count: DataTypes.INTEGER,
-    },
-    {
-        // Other model options go here
-        tableName: "developer",
-        timestamps: false,
-    }
-);
+)
 
 const Employees = db.define(
     "Employees",
@@ -61,10 +40,12 @@ const Employees = db.define(
         tableName: "employees",
         timestamps: false,
     }
-);
+)
 
-const Office = db.define(
-    "Office",
+Employees.hasOne(Office, { foreignKey: "office_id" }) // Foreign key
+
+const Developer = db.define(
+    "Developer",
     {
         id: {
             type: DataTypes.INTEGER,
@@ -72,14 +53,37 @@ const Office = db.define(
             primaryKey: true,
             autoIncrement: true,
         },
-        address: DataTypes.STRING(45),
+        role: DataTypes.STRING(45),
+        level: DataTypes.STRING(45),
+        project_count: DataTypes.INTEGER,
     },
     {
         // Other model options go here
-        tableName: "office",
+        tableName: "developer",
         timestamps: false,
     }
-);
+)
+
+Developer.belongsTo(Employees, { foreignKey: "id" }) // Foreign key
+
+const Clients = db.define(
+    "Clients",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: DataTypes.STRING(45),
+        total_sum: DataTypes.DECIMAL(20, 2),
+    },
+    {
+        // Other model options go here
+        tableName: "clients",
+        timestamps: false,
+    }
+)
 
 const Projects = db.define(
     "Projects",
@@ -104,6 +108,12 @@ const Projects = db.define(
         tableName: "projects",
         timestamps: false,
     }
-);
+)
 
-module.exports = { Clients, Developer, Employees, Office, Projects };
+Projects.hasOne(Developer, { foreignKey: "teamlead_id" }) // Foreign key
+Projects.hasOne(Developer, { foreignKey: "designer_id" }) // Foreign key
+Projects.hasOne(Developer, { foreignKey: "programmer_id" }) // Foreign key
+Projects.hasOne(Developer, { foreignKey: "dbarch_id" }) // Foreign key
+Projects.hasOne(Clients, { foreignKey: "client_id" }) // Foreign key
+
+module.exports = { Clients, Developer, Employees, Office, Projects }
